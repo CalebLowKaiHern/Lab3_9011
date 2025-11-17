8ddf16f788c85ac23f2d1b09a518db8ce8967904
CalebLow <caleblow.23@ichat.sp.edu.sg>
2025-11-17 13:45:00 +0800

    Lab3 should be correct

---
+++ Test_bmi.py
@@ -0,0 +1,16 @@
+import Lab2_6767.bmi as ABC
+
+def test_CalcBMI_underweight():
+        expected = -1
+        result = ABC.calculate_bmi(1.73,37)
+        assert(result == expected)
+
+def test_CalcBMI_normalweight():
+        expected = 0
+        result = ABC.calculate_bmi(1.73,57)
+        assert(result == expected)
+
+def test_CalcBMI_overweight():
+        expected = 1
+        result = ABC.calculate_bmi(1.73,87)
+        assert(result == expected)
